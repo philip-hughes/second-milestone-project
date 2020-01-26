2a79bb6ba2a9b842b8a2f609ab8ab923726b2350
Philip Hughes <phugh80@hotmail.com>
2020-01-26 18:43:33 +0000

    Adding search function that captures search term and redirects to search-results page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,18 @@
 /**
  * Created by Phugh on 25/01/2020.
  */
+
+/* The search function
+    1. Captures the search term and appends it to the url so it can be used on the search-results page.
+    2. Redirects to the search-results page.
+    3. Clears the search box.
+ */
+
+function search(){
+    const searchTerm = $("#searchbox").val();
+    if (searchTerm !== ""){
+        const searchUrl = "search-results.html".concat("?",searchTerm );
+        $(location).attr('href', searchUrl);
+        $("#searchbox").val("");
+    }
+};
