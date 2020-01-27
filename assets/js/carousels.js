8bf7fea63e4c6d9942cd53df4e214f02d1f752dc
Philip Hughes <phugh80@hotmail.com>
2020-01-27 21:25:42 +0000

    Adding carousel functionality that retrives API data, builds elements and inserts them into the DOM, and then runs the Slick scripts. Work in progress

---
+++ assets/js/carousels.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Phugh on 26/01/2020.
+ */
+
+const baseUrl = "https://api.themoviedb.org/3/";
+const apiKey = "057284199444718faf6314cb69a872ab";
+
+const nowPlayingUrl = "".concat(baseUrl, "movie/now_playing?api_key=",apiKey);
+const nowPlayingData = getNowPlaying(nowPlayingUrl);
+
+writeNowPlaying(nowPlayingData);
+writeComingSoon(nowPlayingData);
+
+async function getNowPlaying(url){
+    const data = await fetch(url);
+    return data.json();
+};
+
+
+function writeNowPlaying(data){
+    data.then(data => {
+        const results = data.results;
+        const el = $('#now-playing-carousel');
+        const items = results.map(result => {
+                return `<div class="now-playing-item"><img src="https://image.tmdb.org/t/p/w185${result.poster_path}"></div>`;
+            }
+        );
+        el.append(items);
+        el.slick({
+            infinite: false,
+            slidesToShow: 6,
+            slidesToScroll: 3,
+            nextArrow: $('.np-next'),
+            prevArrow: $('.np-prev')
+        });
+    });
+}
+
+function writeComingSoon(data){
+    data.then(data => {
+        const results = data.results;
+        const el = $('#coming-soon-carousel');
+        const items = results.map(result => {
+                return `<div class="coming-soon-item"><img src="https://image.tmdb.org/t/p/w185${result.poster_path}"></div>`;
+            }
+        );
+        el.append(items);
+        el.slick({
+            infinite: false,
+            slidesToShow: 6,
+            slidesToScroll: 3,
+            nextArrow: $('.cs-next'),
+            prevArrow: $('.cs-prev')
+        });
+    });
+};
+
