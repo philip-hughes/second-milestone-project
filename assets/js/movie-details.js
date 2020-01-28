f503904457aa1d1e3d9d48d418f09296217580c8
Philip Hughes <phugh80@hotmail.com>
2020-01-28 20:40:36 +0000

    Adding functions for fetching movie details and writing results to the DOM. Work in progressassets/js/movie-details.js

--- assets/js/movie-details.js
+++ assets/js/movie-details.js
@@ -1,3 +1,25 @@
 /**
  * Created by Phugh on 28/01/2020.
  */
+const baseUrl = "https://api.themoviedb.org/3/";
+const apiKey = "057284199444718faf6314cb69a872ab";
+
+const movieId = window.location.href.split('?').pop();
+
+const movieDetailsUrl = baseUrl.concat("movie/", movieId, "?api_key=", apiKey, "&language=en-US");
+const movieDetailsData = getMovieDetails(movieDetailsUrl);
+
+writeMovieDetails(movieDetailsData);
+
+async function getMovieDetails(url){
+    const data = await fetch(url);
+    return data.json();
+};
+
+function writeMovieDetails(data){
+   console.dir(data);
+   data.then(data => {
+        const el = $("#movie-details");
+        el.append(`<p>${data.overview}</p>`);
+   })
+};
