a1c6389703ec0b2b142775c575768dddff7acea6
Philip Hughes <phugh80@hotmail.com>
2020-02-04 01:02:29 +0000

    Adding a config file that exports variables that are used by multiple modules

---
+++ assets/js/config.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Phugh on 03/02/2020.
+ */
+export const baseUrl = "https://api.themoviedb.org/3/";
+export const imageBaseUrl = "https://image.tmdb.org/t/p/";
+export const carouselImageSize = "w185/";
+export const searchResultsImageSize = "w92/";
+export const apiKey = "057284199444718faf6314cb69a872ab";
