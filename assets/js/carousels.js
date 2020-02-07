01e71d1adbe82609ff046bd1ae0378b355daeb77
Philip Hughes <phugh80@hotmail.com>
2020-02-07 18:52:48 +0000

    Placing the calls to get the data inside an async function. This resolves a response timing issue that was causing a slight disparity in the initial carousel rendering

--- assets/js/carousels.js
+++ assets/js/carousels.js
@@ -4,23 +4,26 @@
 
 import {apiKey, baseUrl, imageBaseUrl, carouselImageSize} from './config.js';
 
-const nowPlayingUrl = "".concat(baseUrl, "movie/now_playing?api_key=",apiKey);
-const nowPlayingData = getNowPlaying(nowPlayingUrl);
-
+const nowPlayingUrl = "".concat(baseUrl, "movie/now_playing?api_key=",apiKey );
 const comingSoonUrl = "".concat(baseUrl, "movie/upcoming?api_key=", apiKey,"&language=en-US&page=1&region=GB");
-const comingSoonData = getComingSoon(comingSoonUrl);
 
-writeNowPlaying(nowPlayingData);
-writeComingSoon(comingSoonData);
+initCarousels();
+
+async function initCarousels(){
+    const nowPlayingData =  await getNowPlaying(nowPlayingUrl);
+    const comingSoonData =  await getComingSoon(comingSoonUrl);
+    writeNowPlaying(nowPlayingData.json());
+    writeComingSoon(comingSoonData.json());
+}
 
 async function getNowPlaying(url){
     const data = await fetch(url);
-    return data.json();
+    return data;
 };
 
 async function getComingSoon(url){
     const data = await fetch(url);
-    return data.json();
+    return data;
 };
 
 function writeNowPlaying(data){
@@ -28,17 +31,17 @@ function writeNowPlaying(data){
         const results = data.results;
         const el = $('#now-playing-carousel');
         const items = results.map(result => {
-            return `<a href="${"movie-details.html".concat("?",result.id)}">
-                        <div class="now-playing-item"><img src="${imageBaseUrl.concat(carouselImageSize,result.poster_path)}">
+            return `<a class="carousel-item-link" href="${"movie-details.html".concat("?",result.id)}">
+                        <div class="now-playing-item">
+                            <img src="${imageBaseUrl.concat(carouselImageSize,result.poster_path)}">
                         </div>
                     </a>`;
             }
         );
         el.append(items);
-        console.log(items)
         el.slick({
             infinite: false,
-            slidesToShow: 6,
+            slidesToShow: 5,
             slidesToScroll: 3,
             nextArrow: $('.np-next'),
             prevArrow: $('.np-prev')
@@ -51,7 +54,7 @@ function writeComingSoon(data){
         const results = data.results;
         const el = $('#coming-soon-carousel');
         const items = results.map(result => {
-                return `<a href="${"movie-details.html".concat("?",result.id)}">
+                return `<a class="carousel-item-link" href="${"movie-details.html".concat("?",result.id)}">
                             <div class="coming-soon-item">
                                 <img src="${imageBaseUrl.concat(carouselImageSize, result.poster_path)}">
                             </div>
@@ -61,7 +64,7 @@ function writeComingSoon(data){
         el.append(items);
         el.slick({
             infinite: false,
-            slidesToShow: 6,
+            slidesToShow: 5,
             slidesToScroll: 3,
             nextArrow: $('.cs-next'),
             prevArrow: $('.cs-prev')
