2107a370a8b5663e92779dbf8408233f79259aa4
Philip Hughes <phugh80@hotmail.com>
2020-02-12 23:52:42 +0000

    Adding responsive settings to Slick config

--- assets/js/carousels.js
+++ assets/js/carousels.js
@@ -46,7 +46,38 @@ function writeNowPlaying(data){
             slidesToShow: 5,
             slidesToScroll: 3,
             nextArrow: $('.np-next'),
-            prevArrow: $('.np-prev')
+            prevArrow: $('.np-prev'),
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 2,
+                        infinite: false
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 620,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 420,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
     });
 }
@@ -69,7 +100,38 @@ function writeComingSoon(data){
             slidesToShow: 5,
             slidesToScroll: 3,
             nextArrow: $('.cs-next'),
-            prevArrow: $('.cs-prev')
+            prevArrow: $('.cs-prev'),
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 2,
+                        infinite: false
+                    }
+                },
+                {
+                    breakpoint: 800,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 620,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 420,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
     });
 };
